1d87d402a38ff984d06da313b2008b0a63cfb53e
RasmusErik Voel Jensen <gitlab@solsort.com>
2024-01-21 15:30:08 +0000

    wp-client progress

--- wp-client/veduz.js
+++ wp-client/veduz.js
@@ -1,8 +1,116 @@
-console.log('here');
-veduz = self.veduz = self.veduz || {};
-veduz.user = veduz.user || {};
-veduz.store = veduz.store || {};
+(function (self, undefined) {
+  let v = (self.veduz = self.veduz || {});
+  v.user = v.user || {};
+  v.mycrud = v.mycrud || {};
 
+  if (!v.login && !v.logout && !v.wpjson) {
+    function authdata() {
+      let query = location.search
+        ? Object.fromEntries(
+            location.search
+              .slice(1)
+              .split("&")
+              .map((s) => s.split("=").map(decodeURIComponent)),
+          )
+        : {};
+      if (query.site_url && query.user_login && query.password) {
+        localStorage.setItem(
+          "veduz_auth",
+          JSON.stringify({
+            site_url: query.site_url,
+            user_login: query.user_login,
+            password: query.password,
+          }),
+        );
+        location.search = location.search
+          .replace(/&?user_login=[^&]*/, "")
+          .replace(/&?password=[^&]*/, "");
+      }
+      let auth = JSON.parse(localStorage.getItem("veduz_auth") || "{}");
+      auth.site_url = query.site_url || auth.site_url || "https://solsort.com";
+      return auth;
+    }
 
-veduz.user.host = 'solsort.com'
-veduz.user.login = () => location.href = `https://${veduz.user.host}/wp-admin/authorize-application.php?app_name=${encodeURI(document.title + ` [${Math.random().toString(36).slice(2)}]`)}&success_url=${location.href}`
+    const auth = authdata();
+    const login_url = `${auth.site_url}/wp-admin/authorize-application.php?app_name=${encodeURI(document.title + ` [${new Date().toISOString()}]`)}&success_url=${location.href}`;
+    const rest_url = auth.site_url + "/wp-json/";
+
+    v.login = () => (location.href = login_url);
+    v.logout = () => {
+      localStorage.removeItem("veduz_auth");
+      location.reload();
+    };
+    v.wpjson = async (endpoint, opt = {}) =>
+      (
+        await fetch(rest_url + endpoint, {
+          ...opt,
+          headers: {
+            ...opt.headers,
+            Authorization: `Basic ${btoa(`${auth.user_login}:${auth.password}`)}`,
+          },
+        })
+      ).json();
+    v.user.username = auth.user_login;
+  }
+
+  v.user.wp = () => v.wpjson("wp/v2/users/me");
+  v.user.roles = () => v.wpjson("veduz/v1/roles");
+(function (self, undefined) {
+  let v = (self.veduz = self.veduz || {});
+  v.user = v.user || {};
+  v.mycrud = v.mycrud || {};
+
+  if (!v.login && !v.logout && !v.wpjson) {
+    function authdata() {
+      let query = location.search
+        ? Object.fromEntries(
+            location.search
+              .slice(1)
+              .split("&")
+              .map((s) => s.split("=").map(decodeURIComponent)),
+          )
+        : {};
+      if (query.site_url && query.user_login && query.password) {
+        localStorage.setItem(
+          "veduz_auth",
+          JSON.stringify({
+            site_url: query.site_url,
+            user_login: query.user_login,
+            password: query.password,
+          }),
+        );
+        location.search = location.search
+          .replace(/&?user_login=[^&]*/, "")
+          .replace(/&?password=[^&]*/, "");
+      }
+      let auth = JSON.parse(localStorage.getItem("veduz_auth") || "{}");
+      auth.site_url = query.site_url || auth.site_url || "https://solsort.com";
+      return auth;
+    }
+
+    const auth = authdata();
+    const login_url = `${auth.site_url}/wp-admin/authorize-application.php?app_name=${encodeURI(document.title + ` [${new Date().toISOString()}]`)}&success_url=${location.href}`;
+    const rest_url = auth.site_url + "/wp-json/";
+
+    v.login = () => (location.href = login_url);
+    v.logout = () => {
+      localStorage.removeItem("veduz_auth");
+      location.reload();
+    };
+    v.wpjson = async (endpoint, opt = {}) =>
+      (
+        await fetch(`${auth.site_url}/wp-json/wp/v2/users/me`, {
+          ...opt,
+          headers: {
+            ...opt.headers,
+            Authorization: `Basic ${btoa(`${auth.user_login}:${auth.password}`)}`,
+          },
+        })
+      ).json();
+    v.user.username = auth.user_login;
+  }
+
+  v.user.wp = () => v.wpjson("wp/v2/users/me");
+  v.user.roles = () => v.wpjson("veduz/v1/roles");
+})(self);
+})(self);
